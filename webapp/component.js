69f22bfadc13c250bdd1be0386c7cd883eea245c
Unathi Nkomo - BCX <Unathi.Nkomo@bcx.co.za>
2021-05-31 17:53:55 +0200

    ui5

---
+++ webapp/component.js
@@ -0,0 +1,57 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
+    "./controller/HelloDialog",
+    "sap/ui/Device"
+], function (UIComponent, JSONModel, ResourceModel, HelloDialog, Device) {
+    "use strict";
+    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+        metadata: {
+            manifest: "json"
+        },
+        init: function () {
+            // call the init function of the parent
+            UIComponent.prototype.init.apply(this, arguments);
+            // set data models
+            var oData = {
+                recipient: {
+                    name: "BCX"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.setModel(oModel);
+
+            // set device model
+            var oDeviceModel = new JSONModel(Device);
+            oDeviceModel.setDefaultBindingMode("OneWay");
+            this.setModel(oDeviceModel, "device");
+
+            // set dialog
+            this._helloDialog = new HelloDialog(this.getRootControl());
+
+            // create the views based on the url/hash
+            this.getRouter().initialize();
+        },
+
+        getContentDensityClass : function () {
+            if (!this._sContentDensityClass) {
+                if (!Device.support.touch) {
+                    this._sContentDensityClass = "sapUiSizeCompact";
+                } else {
+                    this._sContentDensityClass = "sapUiSizeCozy";
+                }
+            }
+            return this._sContentDensityClass;
+        },
+
+        exit: function () {
+            this._helloDialog.destroy();
+            delete this._helloDialog;
+        },
+
+        openHelloDialog : function () {
+            this._helloDialog.open();
+        } 
+    });
+});
